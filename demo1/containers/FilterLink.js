37f2ea23a30465eff738ac4541372bd8a36831f3
lydisa <385346894@qq.com>
2017-04-11 00:16:00 +0800

    提交

---
+++ demo1/containers/FilterLink.js
@@ -0,0 +1,19 @@
+import  {conect} from 'react-redux'
+import {setVisibilityFilter} from '../action'
+import Link from '../components/Link'
+
+const mapStateToProps = (state,ownProps)=>{
+    return{
+        active:ownProps.filter===state.setVisibilityFilter
+    }
+}
+
+const mapDispatchToProps = (dispatch,ownProps)=>{
+    return{
+        onClick:()=>{
+            dispatch(setVisibilityFilter(ownProps.filter))
+        }
+    }
+}
+
+const FilterLink = connect(mapStateToProps,mapDispatchToProps)(Link)
--- demo1/index.js
+++ demo1/index.js
@@ -1,4 +1,13 @@
-import {createStore} from 'redux'
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
 import todoApp from './reducers'
-
+import App from './components/App'
 let store = createStore(todoApp)
+
+render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root')
+)
